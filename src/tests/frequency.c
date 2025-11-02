b953d8718f4ad26b91d515f9f35f565d4bb0cec1
Mai Thanh Minh <thanhminh.mr@gmail.com>
2025-11-02 23:26:18 +0700

    Add `table.csv` output to `Frequency` test

--- src/tests/frequency.c
+++ src/tests/frequency.c
@@ -59,6 +59,8 @@ static const enum test test_num = TEST_FREQUENCY;	// This test number
  */
 static bool Frequency_print_stat(FILE * stream, struct state *state, struct Frequency_private_stats *stat, double p_value);
 static bool Frequency_print_p_value(FILE * stream, double p_value);
+static bool Frequency_print_table_header(FILE * stream);
+static bool Frequency_print_table_line(FILE * stream, long int iteration, double p_value);
 static void Frequency_metric_print(struct state *state, long int sampleCount, long int toolow, long int *freqPerBin);
 
 
@@ -410,6 +412,81 @@ Frequency_print_p_value(FILE * stream, double p_value)
 
 
 /*
+ * Frequency_print_table_header - print table header to the end of an open file
+ *
+ * given:
+ *      stream          // open writable FILE stream
+ *
+ * returns:
+ *      true --> no errors
+ *      false --> an I/O error occurred
+ */
+static bool
+Frequency_print_table_header(FILE * stream)
+{
+	int io_ret;		// I/O return status
+
+	/*
+	 * Check preconditions (firewall)
+	 */
+	if (stream == NULL) {
+		err(79, __func__, "stream arg is NULL");
+	}
+
+	/*
+	 * Print table header to a file
+	 */
+	io_ret = fputs("iteration,Frequency\n", stream);
+	if (io_ret <= 0) {
+		return false;
+	}
+
+	/*
+	 * All printing successful
+	 */
+	return true;
+}
+
+/*
+ * Frequency_print_table_line - print one table line of information to the end of an open file
+ *
+ * given:
+ *      stream          // open writable FILE stream
+ *      iteration       // the test iteration count
+ *      p_value         // the p_value of the test
+ *
+ * returns:
+ *      true --> no errors
+ *      false --> an I/O error occurred
+ */
+static bool
+Frequency_print_table_line(FILE * stream, long int iteration, double p_value)
+{
+	int io_ret;		// I/O return status
+
+	/*
+	 * Check preconditions (firewall)
+	 */
+	if (stream == NULL) {
+		err(79, __func__, "stream arg is NULL");
+	}
+
+	/*
+	 * Print iteration and p_value to a file
+	 */
+	io_ret = fprintf(stream, "%ld,%f\n", iteration, p_value != NON_P_VALUE ? p_value : 0.0);
+	if (io_ret <= 0) {
+		return false;
+	}
+
+	/*
+	 * All printing successful
+	 */
+	return true;
+}
+
+
+/*
  * Frequency_print - print to results.txt, data*.txt, stats.txt for all iterations
  *
  * given:
@@ -427,9 +504,11 @@ Frequency_print(struct state *state)
 	double p_value;				// p_value iteration test result(s)
 	FILE *stats = NULL;			// Open stats.txt file
 	FILE *results = NULL;			// Open results.txt file
+	FILE *table = NULL;			// Open table.csv file
 	FILE *data = NULL;			// Open data*.txt file
 	char *stats_txt = NULL;			// Pathname for stats.txt
 	char *results_txt = NULL;		// Pathname for results.txt
+	char *table_csv = NULL;			// Pathname for table.csv
 	char *data_txt = NULL;			// Pathname for data*.txt
 	char data_filename[BUFSIZ + 1];		// Basename for a given data*.txt pathname
 	bool ok;				// true -> I/O was OK
@@ -484,6 +563,22 @@ Frequency_print(struct state *state)
 	results = openTruncate(results_txt);
 
 	/*
+	 * Open table.csv file
+	 */
+	table_csv = filePathName(state->subDir[test_num], "table.csv");
+	dbg(DBG_HIGH, "about to open/truncate: %s", table_csv);
+	table = openTruncate(table_csv);
+
+	/*
+	 * Print table header to table.csv
+	 */
+	errno = 0;	// paranoia
+	ok = Frequency_print_table_header(table);
+	if (ok == false) {
+		errp(74, __func__, "error in writing to %s", table_csv);
+	}
+
+	/*
 	 * Write results.txt and stats.txt files
 	 */
 	for (i = 0; i < state->stats[test_num]->count; ++i) {
@@ -514,6 +609,15 @@ Frequency_print(struct state *state)
 		ok = Frequency_print_p_value(results, p_value);
 		if (ok == false) {
 			errp(74, __func__, "error in writing to %s", results_txt);
+		}
+
+		/*
+		 * Print table to table.csv
+		 */
+		errno = 0;	// paranoia
+		ok = Frequency_print_table_line(table, i, p_value);
+		if (ok == false) {
+			errp(74, __func__, "error in writing to %s", table_csv);
 		}
 	}
 
@@ -548,6 +652,22 @@ Frequency_print(struct state *state)
 	}
 	free(results_txt);
 	results_txt = NULL;
+
+	/*
+	 * Flush and close table.csv, free pathname
+	 */
+	errno = 0;		// paranoia
+	io_ret = fflush(table);
+	if (io_ret != 0) {
+		errp(74, __func__, "error flushing to: %s", table_csv);
+	}
+	errno = 0;		// paranoia
+	io_ret = fclose(table);
+	if (io_ret != 0) {
+		errp(74, __func__, "error closing: %s", table_csv);
+	}
+	free(table_csv);
+	table_csv = NULL;
 
 	/*
 	 * Write data*.txt for each data file if we need to partition results
